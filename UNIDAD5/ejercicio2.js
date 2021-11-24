4ea106a51de80ab8f0b5c323abd3e50b289b393a
RubenCastFer <ruben.castellano.fernandez.al@iespoligonosur.org>
2021-11-24 22:38:24 +0100

    ejercicio 2

---
+++ UNIDAD5/ejercicio2.js
@@ -0,0 +1,24 @@
+function iniciar() {
+    const formulario = document.getElementById('registro');
+    formulario.addEventListener('submit', copiarDatos);
+}
+
+function copiarDatos(event) {
+    event.preventDefault();
+    var name = (document.forms["registro"].name.value).toUpperCase();
+    var lastname = (document.forms["registro"].lastname.value).toUpperCase();
+    var username = (document.forms["registro"].username.value).toUpperCase();
+    var date = (document.forms["registro"].date.value).toUpperCase();
+    var sex = (document.forms["registro"].sex.value).toUpperCase();
+    var telefono = (document.forms["registro"].telefono.value).toUpperCase();
+    var email = (document.forms["registro"].email.value).toUpperCase();
+    var password = document.forms["registro"].pass.value;
+    var passwordConfirm = document.forms["registro"].passConf.value;
+    document.write(name + "<br>");
+    document.write(lastname + "<br>");
+    document.write(username + "<br>");
+    document.write(date + "<br>");
+    document.write(sex + "<br>");
+    document.write(telefono + "<br>");
+    document.write(email + "<br>");
+}
