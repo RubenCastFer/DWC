390b9515dbe20a90488333b4b81d1e28f5152b30
RubenCastFer <ruben.castellano.fernandez.al@iespoligonosur.org>
2022-01-11 12:32:49 +0100

    unidad6

---
+++ UNIDAD6/ejercicio1.js
@@ -0,0 +1,35 @@
+window.addEventListener("load",añadir)
+
+function añadir(){
+    var title = document.createElement('title');
+    var txt1 = document.createTextNode('texto');
+    title.appendChild(txt1);
+
+    var p1 = document.createElement('p');
+    var txt2 = document.createTextNode('text');
+    p1.appendChild(txt2);
+
+    var a1 = document.createElement('a');
+    var txt3 = document.createTextNode('text');
+    a1.appendChild(txt3);
+    p1.appendChild(a1);
+
+    var txt4 = document.createTextNode('text');
+    p1.appendChild(txt4);
+
+    var txt5 = document.createTextNode('text');
+    p1.appendChild(txt5);
+
+    var a2 = document.createElement('a');
+    var txt6 = document.createTextNode('text');
+    a2.appendChild(txt6);
+    p1.appendChild(a2);
+
+    var txt7 = document.createTextNode('text');
+    p1.appendChild(txt7);
+    
+
+    document.body.appendChild(p1);
+    document.body.appendChild(h1);
+    document.head.appendChild(title);
+}
---
+++ UNIDAD6/ejercicio2.js
@@ -0,0 +1,21 @@
+window.addEventListener("load",añadir)
+
+function añadir(){
+    
+    var para = document.createElement('p');
+    var txt1 = document.createTextNode('Este es el ');
+    para.appendChild(txt1);
+
+    var enfasis = document.createElement('em');
+    var txt2 = document.createTextNode('contenido');
+    enfasis.appendChild(txt2);
+
+    para.appendChild(enfasis);
+    
+    var txt3 = document.createTextNode(' de mi párrafo');
+    para.appendChild(txt3);
+
+    
+
+    document.getElementById('testdiv').appendChild(para);
+}
