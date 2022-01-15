b0a4dbfd4f306f7c39822f3733ce1d918d9afcc7
RubenCastFer <ruben.castellano.fernandez.al@iespoligonosur.org>
2022-01-15 19:36:49 +0100

    Hasta el 9

---
+++ UNIDAD6/ejercicio6.js
@@ -0,0 +1,18 @@
+window.addEventListener("load",enlace)
+
+function enlace() {
+    var div = document.createElement('div');
+
+    document.body.appendChild(div);
+
+    var enlace = document.createElement('a');
+    enlace.setAttribute("href","ejercicio6.html");
+    var txt = document.createTextNode("Esto es un link");
+
+    enlace.appendChild(txt);
+    div.appendChild(enlace);
+    div.appendChild(document.createElement('br'))
+    var txt = document.createTextNode(document.getElementsByTagName('a')[0].getAttribute("href"));
+    document.getElementsByTagName('div')[0].appendChild(txt);
+   
+}
--- UNIDAD6/ejercicio7.js
+++ UNIDAD6/ejercicio7.js
@@ -1,8 +1,31 @@
-window.addEventListener("load",tabla)
+window.addEventListener("load",inicio)
 
 //añadir el mostrar y el ocultar.
+function inicio(){
+    var enlace = document.createElement('a');
+    enlace.setAttribute("id","mostrarOcultar");
+    //enlace.setAttribute("href","ejercicio7.html");
 
-function tabla() {
+    var txt1 = document.createTextNode("Ocultar/Mostrar");
+    enlace.appendChild(txt1);
+    var div = document.createElement('div');
+    div.appendChild(enlace);
+    document.body.appendChild(div);
+    
+    var elemento1 = document.getElementById("mostrarOcultar");
+    elemento1.addEventListener("click",mostrarOcultar);
+}
+
+function mostrarOcultar(){
+    var tabla=document.getElementsByTagName('table')
+    if (tabla[0]==undefined) {
+        mostrar();
+    }else{
+        tabla[0].remove();
+    }
+}
+
+function mostrar() {
 
     var tabla = document.createElement('table');
     for (let i = 0; i < 20; i++) {
