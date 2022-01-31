cb3c62f96d79359119e6a9a1dd6ff3e9e0eb5c9b
RubenCastFer <ruben.castellano.fernandez.al@iespoligonosur.org>
2022-01-31 14:31:36 +0100

    ajax

---
+++ UNIDAD7/ajax/ajax.php
@@ -0,0 +1,8 @@
+<?php 
+// $resultado = $_POST['valorCaja1'] + $_POST['valorCaja2']; 
+$miArray=["valor1"=>8,"valor1"=>9];
+//echo json_encode([1,2]);
+echo json_encode($miArray); 
+
+//echo $resultado;
+?>
